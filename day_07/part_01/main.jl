c83f1e9252e93b0976006569ebd66f6247503f39
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2024-12-20 11:44:40 -0400

    submitted day 7 with Julia

---
+++ day_07/part_01/main.jl
@@ -0,0 +1,83 @@
+
+# Read the input file as an array of tuples.
+function read_input()
+    lines = []
+
+    for line in readlines("input.txt")
+        test_val, nums = split(line, ": ")
+        nums = split(nums, " ")
+
+        test_val = parse(Int, test_val)
+        nums = map(n -> parse(Int, n), nums)
+
+        push!(lines, (test_val, nums))
+    end
+
+    return lines
+end
+
+# Generates an array of binary arrays representing 
+# all the ways to choose different operators.
+function generate_present_lists(num_amt)
+    lists = []
+    op_amt = num_amt - 1
+
+    for i in range(0, 2^op_amt - 1)
+        bin = digits(i, base=2, pad=op_amt)
+        push!(lists, bin)
+    end
+    return lists
+end
+
+# Calculates the sum of the valid equation lines.
+function sum_of_valid_lines(lines)
+    acc = 0
+
+    for line in lines
+        # Extract the component of the line.
+        test_val, nums = line
+
+        # The amount of numbers in the equation.
+        num_amt = length(nums)
+
+        # The different ways of choosing operators.
+        present_lists = generate_present_lists(num_amt)
+        
+        # Since the first number in `line_acc` is always
+        # the same, we seperate out the rest of the numbers.
+        nums_rest = nums[2:end] # 1-based indexing
+
+        eq_is_valid = false
+        for should_mul in present_lists
+            line_acc = nums[1] # 1-based indexing
+
+            for i in range(1, num_amt - 1)
+                # The `should_mul` array determines what operator 
+                # we choose to perform. 1 means multiplicaiton and 
+                # 0 means addition.
+                if should_mul[i] == 1
+                    line_acc *= nums_rest[i]
+                else
+                    line_acc += nums_rest[i]
+                end
+            end
+
+            # If the result of the equation was the same as the 
+            # test value, then the equation was valid.
+            if line_acc == test_val
+                eq_is_valid = true
+                break
+            end
+        end
+
+        # Add the value of the equation if it was valid.
+        if eq_is_valid
+            acc += test_val
+        end
+    end
+
+    return acc
+end
+
+lines = read_input()
+println(sum_of_valid_lines(lines))
