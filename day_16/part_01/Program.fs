a6263821fba0c303d832398ea1bf70a7c316daf3
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-02-16 14:19:31 -0400

    started working on day 16 in F#

---
+++ day_16/part_01/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+
+let readInput filename = IO.File.ReadAllLines filename
+
+let main () = 
+    let args = Environment.GetCommandLineArgs ()
+
+    let lines = readInput args[1]
+
+    let mutable startPos = -1, -1
+    let mutable endPos = -1, -1
+
+    for r in 0..lines.Length - 1 do
+        for c in 0..lines[0].Length - 1 do
+            match lines[r][c] with
+            | 'S' -> 
+                startPos <- r, c
+            | 'E' -> 
+                endPos <- r, c
+            | _ -> ()
+
+    printfn "%A" startPos
+    printfn "%A" endPos
+
+    // TODO
+    // ...
+
+main ()
