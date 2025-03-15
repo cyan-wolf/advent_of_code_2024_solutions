7afbf81aa487898fadff5a99aa3672b1c00ef2ff
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-03-15 12:53:17 -0400

    continued working on day 5 part 2 in Common Lisp

--- day_05/part_02/main.lisp
+++ day_05/part_02/main.lisp
@@ -1,7 +1,32 @@
 
-(defconstant FILENAME "input_test.txt")
+;; Reads the input from the given filename.
+(defun read-input (filename)
+  (let* ((lines (uiop:read-file-lines filename))
+         (reading-rules t)
+         (rules-list ())
+         (updates-list ()))
+    (progn
+      (loop for line in lines do
+            (if reading-rules
+                ; Parse the rules.
+                (if (string= line "")
+                    (setq reading-rules nil)
+                    (push line rules-list))
+                ; Parse the updates.
+                (let* ((split-line (uiop:split-string line :separator ","))
+                       (nums (mapcar #'parse-integer split-line)))
+                  (push nums updates-list))))
+      ; Return the rules and the list of updates.
+      (values rules-list updates-list)
+      )))
 
+;; Entry point for the program.
 (defun main ()
-  (let* ((lines (uiop:read-file-lines FILENAME)))
-    (loop for line in lines do
-          (print line))))
+  (let ((filename "input_test.txt"))
+    (multiple-value-bind (rules-list updates-list) (read-input filename)
+      ; TODO
+      (progn
+        (print rules-list)
+        (print updates-list))
+
+      )))
