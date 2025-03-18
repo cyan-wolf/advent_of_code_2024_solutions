8af924c93502eb0eaf64255d0d0251328b9d8342
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-03-18 15:03:29 -0400

    continued working on day 5 part 2 in Common Lisp

--- day_05/part_02/main.lisp
+++ day_05/part_02/main.lisp
@@ -1,32 +1,40 @@
-
 ;; Reads the input from the given filename.
 (defun read-input (filename)
   (let* ((lines (uiop:read-file-lines filename))
          (reading-rules t)
-         (rules-list ())
-         (updates-list ()))
+         (rules-map (make-hash-table))
+         (updates-list (list)))
     (progn
       (loop for line in lines do
             (if reading-rules
                 ; Parse the rules.
                 (if (string= line "")
                     (setq reading-rules nil)
-                    (push line rules-list))
+                    (let* ((rules-list (uiop:split-string line :separator "|")) ; Split the line.
+                           ; Parse the integers.
+                           (rules-nums (mapcar #'parse-integer rules-list))
+                           ; Get both parsed numbers.
+                           (num-before (nth 0 rules-nums))
+                           (num-after (nth 1 rules-nums)))
+                      ; Collect all the numbers that should go before `num-after`.
+                      (pushnew num-before (gethash num-after rules-map))))
                 ; Parse the updates.
                 (let* ((split-line (uiop:split-string line :separator ","))
                        (nums (mapcar #'parse-integer split-line)))
                   (push nums updates-list))))
       ; Return the rules and the list of updates.
-      (values rules-list updates-list)
+      (values rules-map updates-list)
       )))
 
 ;; Entry point for the program.
 (defun main ()
   (let ((filename "input_test.txt"))
-    (multiple-value-bind (rules-list updates-list) (read-input filename)
-      ; TODO
-      (progn
-        (print rules-list)
-        (print updates-list))
-
-      )))
+    (multiple-value-bind (rules-map updates-list) (read-input filename)
+      (let ((sum 0))
+        (progn
+          (loop for update in updates-list do
+                ; TODO
+                (print update))
+          
+          (print sum))))
+      ))
