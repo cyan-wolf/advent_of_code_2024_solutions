57ca6015eab3d477c6360518fdb5d76c1bf191b7
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-04-23 14:55:38 -0400

    worked on prolog implementation

--- day_19/part_01/main.pl
+++ day_19/part_01/main.pl
@@ -1,20 +1,39 @@
 :- set_prolog_flag(double_quotes, chars).
 
+% available (?TowelPart)
+% Allows adding new facts at runtime.
+:- dynamic(available/1).
+
 main :-
-    open('input_test.pl', read, Str),
+    % Read input filename from command arguments.
+    current_prolog_flag(argv, [InputFilePath]),
+
+    % Read the input file lines from the input file.
+    open(InputFilePath, read, Str),
     read_file(Str, Lines),
     close(Str),
 
+    % Parse the lines from the input file.
     [AvailableRaw,_|Needed] = Lines,
     split_string(AvailableRaw, ",", " ", Available),
 
+    % Dynamically assert the available towels as... available.
+    forall(member(TowelPart, Available), assertz(available(TowelPart))),
+
+    % TODO: actually use backtracking to build the needed towels using 
+    %       the available towel parts.
+    % ...
+
+    % Debug.
     write(Available), nl,
     write(Needed), nl.
 
+% read_file(+Stream, ?Lines)
+% Reads file lines as a list from the given input stream.
 read_file(Stream, []) :-
     at_end_of_stream(Stream).
 
 read_file(Stream, [Elem|Rest]) :-
     \+ at_end_of_stream(Stream),
     read(Stream, Elem),
-    read_file(Stream, Rest).
+    read_file(Stream, Rest), !.
