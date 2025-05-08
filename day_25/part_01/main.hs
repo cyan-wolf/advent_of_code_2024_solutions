c1de1fff32060a54d0871de58975b5779783c671
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-05-08 13:56:50 -0400

    finished day 25 part 1 in Haskell

--- day_25/part_01/main.hs
+++ day_25/part_01/main.hs
@@ -1,47 +1,68 @@
+import GHC.Num (integerFromInt)
+import System.Environment (getArgs)
 
-data ParseResult = Lock Thing | Key Thing
+-- Represents a fixed sized list of slot heights.
+type SlotHeights = [Integer]
+
+-- Represents whether a list of slot heights belongs to a lock or a key.
+data ParseResult = Lock SlotHeights | Key SlotHeights
                    deriving Show
 
-type Thing = (Integer, Integer, Integer, Integer, Integer)
+-- Processes a list of lines to parse a list of locks and a list of keys.
+process :: [String] -> ([SlotHeights], [SlotHeights])
+process ls = processLines ls [] []
+    where
+        processLines [] locks keys = (locks, keys)
 
-process ls = processLines ls [] [] []
+        processLines (ls :: [String]) (locks :: [SlotHeights]) (keys :: [SlotHeights]) =
+            let remainingInput = drop 8 ls in 
+                case parseSlotHeights (take 7 ls) of
+                    Lock lock -> processLines remainingInput (lock : locks) keys
+                    Key key -> processLines remainingInput locks (key : keys)
 
-processLines [] locks keys _ = (locks, keys)
-
-processLines ((line:ls) :: [String]) (locks :: [Thing]) (keys :: [Thing]) (temp :: [String]) =
-    case line of
-        "" ->
-            case parseTemp temp of
-                Lock lock -> processLines ls (lock : locks) keys []
-                Key key -> processLines ls locks (key : keys) []
-
-        _ -> processLines ls locks keys (line : temp)
-
-parseTemp :: [String] -> ParseResult
-parseTemp (temp :: [String]) =
-    let middle = drop 1 (take 5 temp) in
-        let middleSlots = arrayToFivetuple (countMiddle middle []) in
+-- Checks several lines to parse a lock or a key.
+parseSlotHeights :: [String] -> ParseResult
+parseSlotHeights (temp :: [String]) =
+    let middle = take 5 (drop 1 temp) in
+        let middleSlots = countColumns middle in
             if head temp == "#####" then 
                 Lock middleSlots
             else 
                 Key middleSlots
 
-countMiddle [] acc = acc
+-- Counts how many '#' symbols appear in each position (column) 
+-- of the given rows.
+countColumns :: [String] -> SlotHeights
+countColumns rows =
+    let rowLen = length (head rows) in
+        countMiddleAux rows (replicate rowLen 0)
 
-countMiddle ((t:ts) :: [String]) (acc :: [Integer]) =
-    let additions = map (\c -> if c == '#' then 1 else 0) t in
-        countMiddle ts (zipWith (+) additions acc)
+    where 
+        countMiddleAux [] acc = acc
 
-arrayToFivetuple :: [e] -> (e, e, e, e, e)
-arrayToFivetuple [s1, s2, s3, s4, s5] = (s1, s2, s3, s4, s5)
+        countMiddleAux ((r:rest) :: [String]) (acc :: [Integer]) =
+            let additions = map (\c -> if c == '#' then 1 else 0) r in
+                countMiddleAux rest (zipWith (+) additions acc)
+
+-- Returns whether the given slots fit in each other.
+slotsFit :: SlotHeights -> SlotHeights -> Bool
+slotsFit lock key = 
+    let size = integerFromInt $ length lock in 
+        all (\(lockSlot, keySlot) -> (lockSlot + keySlot) <= size) (zip lock key)
+
+-- Checks every combination of lock and key from the given lists 
+-- and returns how many combinations are non-overlapping.
+findNonOverlappingCombinations locks keys = 
+    length [(lock, key) | lock <- locks, 
+                   key <- keys, 
+                   slotsFit lock key]
 
 main :: IO ()
-main = do
-    contents <- readFile "input_test.txt"
+main = do 
+    args <- getArgs
+    case args of 
+        [filepath] -> do
+            contents <- readFile filepath
+            print (uncurry findNonOverlappingCombinations $ process (lines contents))
 
-    let ls = lines contents
-
-    let (slots, keys) = process ls
-
-    print slots
-    print keys
+        _ -> putStrLn "error: a file path must be provided"
